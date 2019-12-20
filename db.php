3e382119a2e4215e0b052beeb18a13d27bae337c
linga nikhil kumar <nikhilkumar435@gmail.com>
2019-12-20 17:00:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","nikhilkumar","root123","users");
+$con = mysqli_connect("mysql","nikhilkumar","root123","users");
 // Check connection
 if (mysqli_connect_errno())
   {
